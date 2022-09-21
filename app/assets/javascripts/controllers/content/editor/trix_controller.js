bc2695204b97d3acfcc6aa75a028ee129b642ca2
Stephen Nelson <stephen@katalyst.com.au>
2022-09-21 22:39:56 +0930

    FRIN23-18: use H4 instead of H1 for trix-content headings
    Bug: button doesn't work on first interaction with an existing header
    
    fixup !bf9f76b5d24eff23aa7a4948e786319b2ec3fe12

--- app/assets/javascripts/controllers/content/editor/trix_controller.js
+++ app/assets/javascripts/controllers/content/editor/trix_controller.js
@@ -4,9 +4,8 @@ import Trix from "trix";
 export default class TrixController extends Controller {
   trixInitialize(event) {
     const { toolbarElement } = event.target;
-    const input = toolbarElement.querySelector("input[name=href]");
 
-    permitURILinks(input);
+    permitURILinks(toolbarElement);
   }
 }
 
@@ -15,10 +14,79 @@ export default class TrixController extends Controller {
  * input does not permit. Uses a permissive regex pattern which is not suitable
  * for untrusted use cases.
  */
-function permitURILinks(input) {
+function permitURILinks(toolbar) {
+  const input = toolbar.querySelector("input[name=href]");
+
   // Change the input type from "url" to "text"
   input.type = "text";
 
   // restrict acceptable inputs
   input.pattern = "(https?|/|#).*?";
 }
+
+/**
+ * Define an additional heading level that should be accepted by trix.
+ *
+ * @param level the H{level} to define
+ */
+function defineHeading(level) {
+  Trix.config.blockAttributes[`heading${level}`] ||= {
+    tagName: `h${level}`,
+    terminal: true,
+    breakOnReturn: true,
+    group: false,
+  };
+}
+
+// Add heading 4
+defineHeading(4);
+
+// Remove heading 1
+delete Trix.config.blockAttributes.heading1;
+
+/**
+ * Customize default toolbar to modify headings from h1 to h4
+ *
+ * @returns {String} toolbar html fragment
+ */
+Trix.config.toolbar.getDefaultHTML = () => {
+  const { lang } = Trix.config;
+  return `
+<div class="trix-button-row">
+  <span class="trix-button-group trix-button-group--text-tools" data-trix-button-group="text-tools">
+    <button type="button" class="trix-button trix-button--icon trix-button--icon-bold" data-trix-attribute="bold" data-trix-key="b" title="${lang.bold}" tabindex="-1">${lang.bold}</button>
+    <button type="button" class="trix-button trix-button--icon trix-button--icon-italic" data-trix-attribute="italic" data-trix-key="i" title="${lang.italic}" tabindex="-1">${lang.italic}</button>
+    <button type="button" class="trix-button trix-button--icon trix-button--icon-strike" data-trix-attribute="strike" title="${lang.strike}" tabindex="-1">${lang.strike}</button>
+    <button type="button" class="trix-button trix-button--icon trix-button--icon-link" data-trix-attribute="href" data-trix-action="link" data-trix-key="k" title="${lang.link}" tabindex="-1">${lang.link}</button>
+  </span>
+  <span class="trix-button-group trix-button-group--block-tools" data-trix-button-group="block-tools">
+    <button type="button" class="trix-button trix-button--icon trix-button--icon-heading-1" data-trix-attribute="heading4" title="${lang.heading1}" tabindex="-1">${lang.heading1}</button>
+    <button type="button" class="trix-button trix-button--icon trix-button--icon-quote" data-trix-attribute="quote" title="${lang.quote}" tabindex="-1">${lang.quote}</button>
+    <button type="button" class="trix-button trix-button--icon trix-button--icon-code" data-trix-attribute="code" title="${lang.code}" tabindex="-1">${lang.code}</button>
+    <button type="button" class="trix-button trix-button--icon trix-button--icon-bullet-list" data-trix-attribute="bullet" title="${lang.bullets}" tabindex="-1">${lang.bullets}</button>
+    <button type="button" class="trix-button trix-button--icon trix-button--icon-number-list" data-trix-attribute="number" title="${lang.numbers}" tabindex="-1">${lang.numbers}</button>
+    <button type="button" class="trix-button trix-button--icon trix-button--icon-decrease-nesting-level" data-trix-action="decreaseNestingLevel" title="${lang.outdent}" tabindex="-1">${lang.outdent}</button>
+    <button type="button" class="trix-button trix-button--icon trix-button--icon-increase-nesting-level" data-trix-action="increaseNestingLevel" title="${lang.indent}" tabindex="-1">${lang.indent}</button>
+  </span>
+  <span class="trix-button-group trix-button-group--file-tools" data-trix-button-group="file-tools">
+    <button type="button" class="trix-button trix-button--icon trix-button--icon-attach" data-trix-action="attachFiles" title="${lang.attachFiles}" tabindex="-1">${lang.attachFiles}</button>
+  </span>
+  <span class="trix-button-group-spacer"></span>
+  <span class="trix-button-group trix-button-group--history-tools" data-trix-button-group="history-tools">
+    <button type="button" class="trix-button trix-button--icon trix-button--icon-undo" data-trix-action="undo" data-trix-key="z" title="${lang.undo}" tabindex="-1">${lang.undo}</button>
+    <button type="button" class="trix-button trix-button--icon trix-button--icon-redo" data-trix-action="redo" data-trix-key="shift+z" title="${lang.redo}" tabindex="-1">${lang.redo}</button>
+  </span>
+</div>
+<div class="trix-dialogs" data-trix-dialogs>
+  <div class="trix-dialog trix-dialog--link" data-trix-dialog="href" data-trix-dialog-attribute="href">
+    <div class="trix-dialog__link-fields">
+      <input type="url" name="href" class="trix-input trix-input--dialog" placeholder="${lang.urlPlaceholder}" aria-label="${lang.url}" required data-trix-input>
+      <div class="trix-button-group">
+        <input type="button" class="trix-button trix-button--dialog" value="${lang.link}" data-trix-method="setAttribute">
+        <input type="button" class="trix-button trix-button--dialog" value="${lang.unlink}" data-trix-method="removeAttribute">
+      </div>
+    </div>
+  </div>
+</div>
+`;
+};
--- spec/system/katalyst/content/editor/content_spec.rb
+++ spec/system/katalyst/content/editor/content_spec.rb
@@ -34,4 +34,49 @@ RSpec.describe "katalyst/content/editor/content" do
 
     expect(page).to have_link(text: "link", href: "#anchor")
   end
+
+  it "can add a heading" do
+    item = build :katalyst_content_content, content: "testing headings"
+    container = create :page, items: [item]
+
+    visit admin_page_path(container)
+
+    find("a[title='Edit']").click
+
+    # wait for editor to load
+    expect(page).to have_button(text: "Heading")
+
+    find("trix-editor").click
+
+    page.execute_script("document.querySelector('trix-editor').editor.setSelectedRange([0,0])")
+
+    click_on "Heading"
+    click_on "Done"
+
+    expect(page).to have_selector("span", class: "status-text", text: "Unsaved changes", visible: :visible)
+
+    click_on "Publish"
+
+    expect(page).to have_link(class: "status-text", text: "Published", visible: :visible)
+
+    visit page_path(container.slug)
+
+    expect(page).to have_css("h4", text: "testing headings")
+  end
+
+  # regression test for headings not being detected on first interaction
+  it "can modify an existing heading" do
+    item = build :katalyst_content_content, content: "<h4>testing headings</h4>"
+    container = create :page, items: [item]
+
+    visit admin_page_path(container)
+
+    find("a[title='Edit']").click
+
+    # wait for editor to load
+    expect(page).to have_css("trix-editor h4")
+
+    # ensure heading has correctly identified that the first element is a heading
+    expect(page).to have_selector("button[data-trix-attribute=heading4]", class: "trix-active")
+  end
 end
