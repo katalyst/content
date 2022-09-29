7d02c0245e1079911455807d8c32ee28afba99d7
Sughosh Rao <sughosh.rao@katalyst.com.au>
2022-09-29 11:57:27 +0930

    FRIN23-103: Allow item factory to support nested children

--- spec/factories/katalyst/content/items.rb
+++ spec/factories/katalyst/content/items.rb
@@ -5,12 +5,22 @@ FactoryBot.define do
     heading { Faker::Lorem.word }
     show_heading { true }
     background { Katalyst::Content.config.backgrounds.sample }
+    depth { 0 } # Used for nesting items in system specs
   end
 
   factory :katalyst_content_content, class: "Katalyst::Content::Content" do
     heading { Faker::Lorem.word }
     show_heading { true }
     background { Katalyst::Content.config.backgrounds.sample }
+    depth { 0 } # Used for nesting items in system specs
+
     content { Faker::Hacker.say_something_smart }
+  end
+
+  factory :katalyst_content_section, class: "Katalyst::Content::Section" do
+    heading { Faker::Lorem.word }
+    show_heading { true }
+    background { Katalyst::Content.config.backgrounds.sample }
+    depth { 0 } # Used for nesting items in system specs
   end
 end
