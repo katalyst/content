f26f60677f0072fe0cf7038b8752427fc7a3fd44
Hasarinda Thenuwara <hasarinda.thenuwara@katalyst.com.au>
2022-10-07 09:11:49 +1030

    Refactor: Add item examples to remove duplicate specs

--- spec/models/katalyst/content/item_spec.rb
+++ spec/models/katalyst/content/item_spec.rb
@@ -5,25 +5,13 @@ require "rails_helper"
 RSpec.describe Katalyst::Content::Item do
   subject(:item) { build :katalyst_content_item, container: Page.new }
 
-  it { is_expected.to be_valid }
-
-  it { is_expected.to belong_to(:container).required }
-
-  it { is_expected.to validate_presence_of(:heading) }
-  it { is_expected.to validate_presence_of(:background) }
-  it { is_expected.to validate_inclusion_of(:background).in_array(Katalyst::Content.config.backgrounds) }
+  it_behaves_like "a item"
 
   describe "#to_plain_text" do
     it { is_expected.to have_attributes(to_plain_text: item.heading) }
 
     context "when heading is hidden" do
       subject(:item) { build :katalyst_content_item, container: Page.new, show_heading: false }
-
-      it { is_expected.to have_attributes(to_plain_text: nil) }
-    end
-
-    context "when item is not visible" do
-      subject(:item) { build :katalyst_content_item, container: Page.new, visible: false }
 
       it { is_expected.to have_attributes(to_plain_text: nil) }
     end
