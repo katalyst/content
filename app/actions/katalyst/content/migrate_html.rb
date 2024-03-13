617987705abf2bf1c6c9f85b6e8adb6b00d15783
Hasarinda Thenuwara <hasarinda.thenuwara@katalyst.com.au>
2024-03-13 12:55:09 +1030

    Update MigrateHtml to a mixin module

--- app/actions/katalyst/content/migrate_html.rb
+++ app/actions/katalyst/content/migrate_html.rb
@@ -3,44 +3,17 @@
 # rubocop:disable Metrics/PerceivedComplexity
 module Katalyst
   module Content
-    class MigrateHtml
-      include ActiveModel::Model
+    module MigrateHtml
+      extend ActiveSupport::Concern
 
       SUPPORTED_ROOT_TAGS = %w[br h4 h5 h6 hr ol p text ul].freeze
       SUPPORTED_TRIX_TAGS = %w[h4 h5 h6 hr ol p text ul li b br em a strong span].freeze
 
-      attr_reader :model
-
-      def self.call(model, content)
-        new.call(model, content)
-      end
-
-      delegate :config, to: Katalyst::Content::Config
-
-      def call(model, content)
-        @model = model
-        @items = []
-        @depth = 0
-
+      def add_html(content)
         root = Nokogiri::HTML5.fragment(content)
 
         root.children.each do |node|
-          case node.name
-          when "h2"
-            if node.text.strip.present?
-              add_section_node(heading: node.text.strip, heading_style: "default")
-            else
-              add_section_node(heading: "Empty section", heading_style: "none")
-            end
-          when "h3"
-            add_content_node(heading: node.text, heading_style: "default")
-          when "br", "h4", "h5", "h6", "hr", "ol", "p", "text", "ul"
-            append_html(node)
-          when "table"
-            add_table_node(node)
-          else
-            errors.add(:base, "contains invalid tag #{node.name}")
-          end
+          add_node(node)
         end
 
         @items.each do |item|
@@ -49,26 +22,45 @@ module Katalyst
           end
         end
 
-        unless @model.save
-          errors.copy!(@model)
-
-          return self
-        end
-
-        @model.items_attributes = @items.map.with_index do |item, index|
-          { id: item.id, index:, depth: item.depth }
-        end
-
-        @model.publish!
-
         self
       end
 
-      def success?
-        errors.empty?
+      def add_node(node)
+        case node.name
+        when "h2"
+          if node.text.strip.present?
+            add_section_node(heading: node.text.strip, heading_style: "default")
+          else
+            add_section_node(heading: "Empty section", heading_style: "none")
+          end
+        when "h3"
+          add_content_node(heading: node.text, heading_style: "default")
+        when "br", "h4", "h5", "h6", "hr", "ol", "p", "text", "ul"
+          append_html(node)
+        when "table"
+          add_table_node(node)
+        else
+          errors.add(:base, "contains invalid tag #{node.name}")
+        end
       end
 
-      private
+      def append_html(node)
+        content = last_content_node
+
+        node.traverse do |n|
+          errors.add(:base, "contains invalid tag #{n.name}") unless SUPPORTED_TRIX_TAGS.include?(n.name)
+        end
+
+        content.content = if content.content.present?
+                            content.content.read_attribute_before_type_cast(:body) + node.to_html
+                          else
+                            node.to_html
+                          end
+
+        content.heading ||= heading_for(content.content)
+
+        content
+      end
 
       def build(type:, **)
         @last = item = @model.items.build(
@@ -79,6 +71,10 @@ module Katalyst
         @items << item
         item
       end
+
+      delegate :config, to: Katalyst::Content::Config
+
+      private
 
       def add_section_node(heading:, **)
         heading = heading.strip
@@ -105,7 +101,7 @@ module Katalyst
           **,
         )
 
-        errors.copy!(table)
+        errors.copy!(table.errors)
 
         table.heading ||= heading_for(table)
 
@@ -118,24 +114,6 @@ module Katalyst
           **defaults,
           **,
         )
-      end
-
-      def append_html(node)
-        content = last_content_node
-
-        node.traverse do |n|
-          errors.add(:base, "contains invalid tag #{n.name}") unless SUPPORTED_TRIX_TAGS.include?(n.name)
-        end
-
-        content.content = if content.content.present?
-                            content.content.read_attribute_before_type_cast(:body) + node.to_html
-                          else
-                            node.to_html
-                          end
-
-        content.heading ||= heading_for(content.content)
-
-        content
       end
 
       def last_content_node(**)
