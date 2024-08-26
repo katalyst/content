51df50d012944116cf4b36e1d00409a21aa739b2
Stephen Nelson <stephen@katalyst.com.au>
2024-08-26 13:59:21 +0930

    Add preview route to dummy app frontend

--- spec/dummy/app/controllers/pages_controller.rb
+++ spec/dummy/app/controllers/pages_controller.rb
@@ -4,12 +4,16 @@ class PagesController < ApplicationController
   helper Katalyst::Content::FrontendHelper
 
   def show
-    render locals: { page: }
+    render locals: { page:, version: page.published_version }
+  end
+
+  def preview
+    render :show, locals: { page:, version: page.draft_version }
   end
 
   private
 
   def page
-    @page ||= Page.find_by!(slug: params[:slug])
+    @page ||= Page.find_by!(slug: params[:slug] || params[:page_slug])
   end
 end
