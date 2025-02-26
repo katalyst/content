80d4289f39782a99f60d6066751b16e550422680
Stephen Nelson <stephen@katalyst.com.au>
2025-02-27 09:15:55 +1030

    Move require to lib to address eager load issue
    CI=true build failing for Koi due to load order.
    Seems to be caused by a an upstream change to
    active_storage_validations.

--- app/models/katalyst/content/figure.rb
+++ app/models/katalyst/content/figure.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "active_storage_validations"
-
 module Katalyst
   module Content
     class Figure < Item
--- lib/katalyst/content.rb
+++ lib/katalyst/content.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "active_storage_validations"
 require "active_support"
 
 module Katalyst
